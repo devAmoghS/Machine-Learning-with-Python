7954ee96ef70f7ebd1c07ec45b97dcbfbc520318
aditi15053 <chavanaditi1515@gmail.com>
2023-10-16 17:56:17 +0530

    Some Improvements and comments

--- k_means_clustering/model.py
+++ k_means_clustering/model.py
@@ -1,42 +1,37 @@
 import random
-
 from k_means_clustering.data import inputs
-from k_means_clustering.utils import KMeans, bottom_up_cluster, \
-    generate_clusters, get_values
+from k_means_clustering.utils import KMeans, bottom_up_cluster, generate_clusters, get_values
 
 if __name__ == '__main__':
+    # Set the random seed for reproducibility
     random.seed(0)
-    cluster = KMeans(3)
-    cluster.train(inputs=inputs)
+
+    # Perform k-means clustering with k=3
+    kmeans_3 = KMeans(3)
+    kmeans_3.train(inputs=inputs)
     print("3-means:")
-    print(cluster.means)
+    print(kmeans_3.means)
     print()
 
-    random.seed(0)
-    cluster = KMeans(2)
-    cluster.train(inputs=inputs)
+    # Perform k-means clustering with k=2
+    kmeans_2 = KMeans(2)
+    kmeans_2.train(inputs=inputs)
     print("2-means:")
-    print(cluster.means)
+    print(kmeans_2.means)
     print()
 
-    # for k in range(1, len(inputs) + 1):
-    #     print(k, squared_clustering_errors(inputs, k))
-    # print()
-
-    # recolor_image('/home/amogh/Pictures/symantec.png')
-
-    print("bottom up hierarchical clustering")
-
+    # Perform hierarchical clustering
+    print("Bottom-up hierarchical clustering")
     base_cluster = bottom_up_cluster(inputs)
     print(base_cluster)
 
-    print()
-    print("three clusters, min:")
+    # Generate three clusters with the minimum size
+    print("Three clusters, min:")
     for cluster in generate_clusters(base_cluster, 3):
         print(get_values(cluster))
 
-    print()
-    print("three clusters, max:")
+    # Generate three clusters with the maximum size
+    print("Three clusters, max:")
     base_cluster = bottom_up_cluster(inputs, max)
     for cluster in generate_clusters(base_cluster, 3):
         print(get_values(cluster))
